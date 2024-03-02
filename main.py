716f0fd11db6891e4d9f32d90b9e478321d1110f
Davi Nascimento <davileite2007@gmail.com>
2024-03-02 13:40:21 -0300

    Looping eterno O programa roda pelo tempo que o usuário quiser. Para interromper, basta apertar Ctrl + C.

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from modulos.objetos import estados, dias
 from time import sleep
 
-for dia in dias:
-    for dicio in estados:
-        estado = list(dicio.keys())[0]
-        centros = dicio[estado]
+print('''\033[33m
+<<< CTRL + C para interromper >>>\033[m''')
+while True:
+    try:
+        for dia in dias:
+            for dicio in estados:
+                estado = list(dicio.keys())[0]
+                centros = dicio[estado]
+                print('''
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        ''')
+                print(f'''\033[36m{dia}\033[m - \033[35m{estado.estado.upper()}\033[m'''.center(52))
+                print()
+                sleep(1)
+
+                for centro in centros:
+                    estado.transporte.enviar_itens(centro)
+                    centro.entrega.fazer_entrega()
+                    sleep(2)
+    except KeyboardInterrupt:
         print('''
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-''')
-        print(f'''\033[36m{dia}\033[m - \033[35m{estado.estado.upper()}\033[m'''.center(52))
+        ''')
+        print('\033[36m<<< FIM DA SIMULAÇÃO >>>\033[m')
         print()
-        sleep(1)
-
-        for centro in centros:
-            estado.transporte.enviar_itens(centro)
-            centro.entrega.fazer_entrega()
-            sleep(2)
-
-print('<<< FIM DA SIMULAÇÃO >>>')
-print()
+        break
