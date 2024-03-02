d114b0f1ff1a1f574388fa77eacee40c23a8cfdb
Davi Nascimento <davileite2007@gmail.com>
2024-03-02 10:46:12 -0300

    Tranferência de arquivos

--- conversor.py
+++
@@ -1,8 +0,0 @@
-filename = '50000lines_itens.txt'
-itens = []
-with open(filename) as f:
-    for linha in f:
-        itens.append(linha.strip())
-
-with open('10000_itens.txt', 'a') as f:
-    f.write(str(itens[:10000]))
